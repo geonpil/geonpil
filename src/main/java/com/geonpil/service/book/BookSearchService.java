e14b319424d4fef1f8f4b1f8c8389df0fb09a601
gyuwon <lwd33021@naver.com>
2025-05-24 21:01:01 +0900

    책 검색기능 + 페이지네이션 구현

--- src/main/java/com/geonpil/service/book/BookSearchService.java
+++ src/main/java/com/geonpil/service/book/BookSearchService.java
@@ -1,9 +1,11 @@
-package com.geonpil.service;
+package com.geonpil.service.book;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.geonpil.domain.Book;
+import com.geonpil.dto.response.BookSearchResponse;
+import com.geonpil.dto.response.Meta;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
@@ -27,10 +29,12 @@ public class BookSearchService {
 
     private final RestTemplate restTemplate = new RestTemplate();
 
-    public List<Book> searchBooks(String query) {
+    public BookSearchResponse searchBooks(String query,int page, int size) {
         String url = UriComponentsBuilder
                 .fromHttpUrl("https://dapi.kakao.com/v3/search/book")
                 .queryParam("query", query)
+                .queryParam("page", page)
+                .queryParam("size", size)
                 .build()
                 .toUriString();
 
@@ -46,10 +50,21 @@ public class BookSearchService {
         try{
             ObjectMapper mapper = new ObjectMapper();
             JsonNode root = mapper.readTree(body);
+
             JsonNode docs = root.path("documents");
+            JsonNode metaNode = root.path("meta");
+
+            List<Book> books = Arrays.asList(mapper.treeToValue(docs, Book[].class));
+            Meta meta = mapper.treeToValue(metaNode,Meta.class);
 
 
-            return Arrays.asList(mapper.treeToValue(docs, Book[].class));
+            BookSearchResponse result = new BookSearchResponse();
+            result.setDocuments(books);
+            result.setMeta(meta);
+
+
+            return result;
+
         } catch (JsonProcessingException e) {
             throw new RuntimeException("JSON 파싱 실패", e);
         }
