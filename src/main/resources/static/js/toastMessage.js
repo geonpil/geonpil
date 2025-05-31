5e4e231eff9e2139c4af2f561518f64d8f4e852a
gyuwon <lwd33021@naver.com>
2025-05-31 20:57:38 +0900

    리뷰등록시 등록 알림 alert를 toast로 대체

---
+++ src/main/resources/static/js/toastMessage.js
@@ -0,0 +1,9 @@
+function showToast(message) {
+    const toast = document.getElementById('toast');
+    toast.textContent = message;
+    toast.classList.add('show');
+
+    setTimeout(() => {
+        toast.classList.remove('show');
+    }, 2500); // 2.5초 후 사라짐
+}
