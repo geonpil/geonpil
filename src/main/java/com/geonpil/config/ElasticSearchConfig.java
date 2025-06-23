fd4718a01b070d4b77765fa1aa974488a9861b05
gyuwon <lwd33021@naver.com>
2025-06-23 21:44:05 +0900

    엘라스틱 서치 옵션 변경

--- src/main/java/com/geonpil/config/ElasticSearchConfig.java
+++ src/main/java/com/geonpil/config/ElasticSearchConfig.java
@@ -3,35 +3,25 @@ package com.geonpil.config;
 import com.geonpil.elasticsearch.BoardDocument;
 import com.geonpil.elasticsearch.ContestDocument;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
 import org.springframework.data.elasticsearch.core.convert.ElasticsearchConverter;
 import org.springframework.data.elasticsearch.core.convert.MappingElasticsearchConverter;
 import org.springframework.data.elasticsearch.core.mapping.SimpleElasticsearchMappingContext;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
-import co.elastic.clients.elasticsearch.ElasticsearchClient;
-import co.elastic.clients.json.jackson.JacksonJsonpMapper;
-import co.elastic.clients.transport.ElasticsearchTransport;
-import co.elastic.clients.transport.rest_client.RestClientTransport;
-import org.apache.http.HttpHost;
-import org.elasticsearch.client.RestClient;
 import org.springframework.context.annotation.Bean;
 
 import java.util.Set;
 
 @Configuration
 @EnableElasticsearchRepositories(basePackages = "com.geonpil.repository.search")
-public class ElasticSearchConfig {
+public class ElasticSearchConfig extends ElasticsearchConfiguration {
 
-    @Bean
-    public ElasticsearchClient elasticsearchClient() {
-        RestClient restClient = RestClient.builder(
-                new HttpHost("localhost", 9200, "http"))
+    @Override
+    public ClientConfiguration clientConfiguration() {
+        return ClientConfiguration.builder()
+                .connectedTo("localhost:9200")
                 .build();
-
-        ElasticsearchTransport transport = new RestClientTransport(
-                restClient, new JacksonJsonpMapper());
-
-        return new ElasticsearchClient(transport);
     }
 
     @Bean
