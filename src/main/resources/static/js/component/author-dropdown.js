84f162380aee957235976746819d703f30c0a1a3
gyuwon <lwd33021@naver.com>
2025-07-29 23:42:30 +0900

    wip : 작성자가 쓴글보기

---
+++ src/main/resources/static/js/component/author-dropdown.js
@@ -0,0 +1,19 @@
+(function(){
+  // 드롭다운 토글
+  document.addEventListener('click', e=>{
+    const toggler = e.target.closest('.author-name');
+    const all = document.querySelectorAll('.author-dropdown');
+    all.forEach(el=>el.classList.remove('open'));
+
+    if(toggler){
+      e.preventDefault();
+      toggler.parentElement.classList.toggle('open');
+      e.stopPropagation();
+    }
+  });
+  // 바깥 클릭 시 드롭다운 닫기
+  document.addEventListener('click', ()=> {
+    document.querySelectorAll('.author-dropdown')
+            .forEach(el=>el.classList.remove('open'));
+  });
+})();
